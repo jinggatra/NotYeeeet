c818df909231772cb2ed603c1586b15056c4aa60
J I N G G A <J I N G G A@DESKTOP-FVRNFBC>
2018-10-17 22:48:54 +0700

    Pada class ini menambahkan atribut item, discountItem, dan promoItem dari Class Item, DiscountItem, dan PromoItem

--- src/utspbo/Transaction.java
+++ src/utspbo/Transaction.java
@@ -12,14 +12,25 @@ package utspbo;
  */
 public class Transaction {
     
-	public void print() {
-		System.out.println("| Nama \t\t| Harga \t| Jumlah \t| Total \t|");
-		System.out.println("|---------------------------------------------------------------|");
-		System.out.println(item);
-		System.out.println(discountItem);
-		System.out.println(promoItem);
-		System.out.println("|---------------------------------------------------------------|");
-	}
+    private Item item;
+    private DiscountItem discountItem;
+    private PromoItem promoItem;
+
+    public Transaction(Item item, DiscountItem discountItem, PromoItem promoItem) {
+        this.item = item;
+        this.discountItem = discountItem;
+        this.promoItem = promoItem;
+    }
+    
+    
+    public void print() {
+	System.out.println("| Nama \t\t| Harga \t| Jumlah \t| Total \t|");
+	System.out.println("|---------------------------------------------------------------|");
+	System.out.println(item);
+	System.out.println(discountItem);
+	System.out.println(promoItem);
+	System.out.println("|---------------------------------------------------------------|");
+    }
 	
 }
 
